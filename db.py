740cdc67a84146e1c2bb281d32bb8804cd1c465c
George <gstykalin@mail.ru>
2023-02-16 23:46:45 +0300

    add collections creating

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pymongo
 from dotenv import load_dotenv
 
@@ -10,8 +11,19 @@ class Mongo:
     username = os.getenv('MONGO_INITDB_ROOT_USERNAME')
     password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
     address = os.getenv("ADDRESS")
+    database_name = 'printers'
+    collections_data_file = './collections_data.json'
     
     def __init__(self) -> None:
-        connection_string = f'mongodb://{Mongo.username}:{Mongo.password}@{Mongo.address}/printers?retryWrites=true'
+        connection_string = f'mongodb://{self.username}:{self.password}@{self.address}/{self.database_name}?retryWrites=true'
         client = pymongo.MongoClient(connection_string)
         self.db = client.get_database()
+        # self._create_collections()
+
+    def _create_collections(self) -> None:
+        with open('collections_data_file', 'r') as file:
+            collections_data = json.load(file)
+            
+        for collection_data in collections_data:
+            self.db.create_collection(collection_data['collection_name'], 
+                                      validator=collection_data['validator'])
